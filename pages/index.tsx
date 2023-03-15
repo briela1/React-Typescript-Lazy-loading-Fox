6fe0b7b7bb2fa3542063f4037770a57edcea26b8
briela1 <98447320+briela1@users.noreply.github.com>
2023-03-14 19:28:39 -0500

    state con tipos primitivos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import { useState } from 'react';
 import { RandomFox } from '../components/RandomFox'
 
+const random = () => Math.floor(Math.random() * 123) + 1;
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
   return (
     <>
       <Head>
@@ -16,9 +23,14 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">
-        Hello world!
+          Hello world!
         </h1>
-        <RandomFox />
+        {images.map((image, index) => (
+          <div key={index} className="p-4">
+            <RandomFox image={image} />
+          </div>
+        ))}
+        
       </main>
       <footer></footer>
     </>
