7739b48947c7a24154dcfa17cae124502bde3197
briela1 <98447320+briela1@users.noreply.github.com>
2023-03-16 10:48:16 -0500

    lazy loading

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,11 +1,35 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
     const node = useRef<HTMLImageElement>(null) //inicializamos en null
     
+    const [src, setSrc] = useState(
+        "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+      );
+    
+      useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            if (!entry.isIntersecting || !node.current) {
+              return;
+            }
+    
+            setSrc(image);
+          });
+        });
+    
+        if (node.current) {
+          observer.observe(node.current);
+        }
+    
+        return () => {
+          observer.disconnect();
+        };
+      }, [image]);
+    
     return (
-        <img ref={node} src={image} width={320} height="auto" className="rounded" />
+        <img ref={node} src={src} width={320} height="auto" className=" max-auto rounded-md bg-gray-300" />
     )
 }
